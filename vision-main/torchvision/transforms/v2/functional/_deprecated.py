439a170372718cd708eb3ae5c336acc27157dfff
Payuphat Boontaokaew <125558332+PayuphatBoontaokaew@users.noreply.github.com>
2023-07-06 13:41:04 +0700

    1

---
+++ vision-main/torchvision/transforms/v2/functional/_deprecated.py
@@ -0,0 +1,24 @@
+import warnings
+from typing import Any, List, Union
+
+import torch
+
+from torchvision import datapoints
+from torchvision.transforms import functional as _F
+
+
+@torch.jit.unused
+def to_tensor(inpt: Any) -> torch.Tensor:
+    warnings.warn(
+        "The function `to_tensor(...)` is deprecated and will be removed in a future release. "
+        "Instead, please use `to_image_tensor(...)` followed by `convert_image_dtype(...)`."
+    )
+    return _F.to_tensor(inpt)
+
+
+def get_image_size(inpt: Union[datapoints._ImageTypeJIT, datapoints._VideoTypeJIT]) -> List[int]:
+    warnings.warn(
+        "The function `get_image_size(...)` is deprecated and will be removed in a future release. "
+        "Instead, please use `get_spatial_size(...)` which returns `[h, w]` instead of `[w, h]`."
+    )
+    return _F.get_image_size(inpt)
