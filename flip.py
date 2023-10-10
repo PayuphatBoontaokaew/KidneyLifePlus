eeb4f2c394f4dcc5365b65f5d44e6806a38bac0d
Payuphat Boontaokaew <125558332+PayuphatBoontaokaew@users.noreply.github.com>
2023-10-10 13:37:32 +0700

    picture

--- flip.py
+++ flip.py
@@ -1,10 +1,22 @@
 import cv2
 import os
+import imgaug as ia
+from imgaug import augmenters as iaa
 
-input_folder = "/Users/payuphatboontaokaew/Documents/GitHub/KidneyLifePlus/moder"  # Change this to the folder containing your input images
-output_folder = "/Users/payuphatboontaokaew/Documents/GitHub/KidneyLifePlus/moder"  # Change this to the folder where you want to save the rewinded images
+input_folder = "/Users/payuphatboontaokaew/Documents/GitHub/KidneyLifePlus/Cataract"  # Change this to the folder containing your input images
+output_folder = "/Users/payuphatboontaokaew/Documents/GitHub/KidneyLifePlus/Cataract-Aug"  # Change this to the folder where you want to save the augmented images
+augmented_images_per_input = 4  # Set the number of augmented images per input image
 
-def rewind_images(input_folder, output_folder):
+# Define data augmentation parameters
+augmentation = iaa.Sequential([
+    iaa.Fliplr(0.5),          # Horizontal flip with a 50% chance
+    iaa.Flipud(0.5),          # Vertical flip with a 50% chance
+    iaa.Rotate((-45, 45)),   # Rotate images by -45 to 45 degrees
+    iaa.GaussianBlur(sigma=(0, 1.0)),  # Apply Gaussian blur with a random sigma
+    iaa.Add((-10, 10), per_channel=0.5),  # Add random brightness (-10 to 10) to 50% of the images
+])
+
+def augment_and_save_images(input_folder, output_folder):
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -22,24 +34,17 @@ def rewind_images(input_folder, output_folder):
             print(f"Unable to read {image_file}. Skipping.")
             continue
 
-        # Create rewinded versions
-        rewinded_left = cv2.flip(original_image, 1)
-        rewinded_right = cv2.flip(original_image, 0)
-        rewinded_up = cv2.flip(rewinded_left, 0)
-        rewinded_down = cv2.flip(rewinded_right, 1)
+        # Generate the specified number of augmented images per input image
+        for i in range(augmented_images_per_input):
+            # Apply data augmentation to the original image
+            augmented_images = augmentation(images=[original_image])
 
-        # Save the rewinded images
-        left_output_path = os.path.join(output_folder, f"left_{image_file}")
-        right_output_path = os.path.join(output_folder, f"right_{image_file}")
-        up_output_path = os.path.join(output_folder, f"up_{image_file}")
-        down_output_path = os.path.join(output_folder, f"down_{image_file}")
+            # Save the augmented images with unique filenames
+            for j, augmented_image in enumerate(augmented_images):
+                output_path = os.path.join(output_folder, f"aug_{i}_{j}_{image_file}")
+                cv2.imwrite(output_path, augmented_image)
 
-        cv2.imwrite(left_output_path, rewinded_left)
-        cv2.imwrite(right_output_path, rewinded_right)
-        cv2.imwrite(up_output_path, rewinded_up)
-        cv2.imwrite(down_output_path, rewinded_down)
-
-        print(f"Rewound {image_file} in all directions.")
+                print(f"Augmented {image_file} and saved as {output_path}")
 
 if __name__ == "__main__":
-    rewind_images(input_folder, output_folder)
+    augment_and_save_images(input_folder, output_folder)
