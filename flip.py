581c582a50229e15479957a1619c9aa89e1e9d88
Payuphat Boontaokaew <125558332+PayuphatBoontaokaew@users.noreply.github.com>
2023-11-03 12:02:02 +0700

    update

--- flip.py
+++ flip.py
@@ -3,43 +3,47 @@ import os
 import imgaug as ia
 from imgaug import augmenters as iaa
 
-input_folder = "/Users/payuphatboontaokaew/Documents/GitHub/KidneyLifePlus/Cataract"  # Change this to the folder containing your input images
-output_folder = "/Users/payuphatboontaokaew/Documents/GitHub/KidneyLifePlus/Cataract-Aug"  # Change this to the folder where you want to save the augmented images
-augmented_images_per_input = 4  # Set the number of augmented images per input image
+input_folder = "/Users/payuphatboontaokaew/Documents/GitHub/KidneyLifePlus/Moderate_Demented"
+output_folder = "/Users/payuphatboontaokaew/Documents/GitHub/KidneyLifePlus/Moderate-Alz-Aug"
+augmented_images_per_input = 20
 
-# Define data augmentation parameters
+# Define the desired size
+import imgaug.augmenters as iaa
+
+desired_size = (640, 640)
+
 augmentation = iaa.Sequential([
-    iaa.Fliplr(0.5),          # Horizontal flip with a 50% chance
-    iaa.Flipud(0.5),          # Vertical flip with a 50% chance
-    iaa.Rotate((-45, 45)),   # Rotate images by -45 to 45 degrees
-    iaa.GaussianBlur(sigma=(0, 1.0)),  # Apply Gaussian blur with a random sigma
-    iaa.Add((-10, 10), per_channel=0.5),  # Add random brightness (-10 to 10) to 50% of the images
+    iaa.Resize({"height": desired_size[0], "width": desired_size[1]}),
+    iaa.Fliplr(0.5),
+    iaa.Flipud(0.5),
+    iaa.Affine(rotate=(-90, 90)),
+    iaa.Affine(rotate=180),
+    # iaa.Affine(translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}),
+    # iaa.Affine(scale=(0.66, 1.34)),  # Equivalent to 34% maximum zoom
+    # iaa.Multiply((0.5, 1.5), per_channel=0.5),
+    # iaa.Add((-35, 35)),
+    # iaa.GaussianBlur(sigma=(0, 2.0)),
+    # iaa.SaltAndPepper(0.02),  # 2% noise
 ])
 
+
 def augment_and_save_images(input_folder, output_folder):
-    # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Get a list of all image files in the input folder
     image_files = [f for f in os.listdir(input_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
 
     for image_file in image_files:
-        # Read the image
         image_path = os.path.join(input_folder, image_file)
         original_image = cv2.imread(image_path)
 
-        # Check if the image was read successfully
         if original_image is None:
             print(f"Unable to read {image_file}. Skipping.")
             continue
 
-        # Generate the specified number of augmented images per input image
         for i in range(augmented_images_per_input):
-            # Apply data augmentation to the original image
             augmented_images = augmentation(images=[original_image])
 
-            # Save the augmented images with unique filenames
             for j, augmented_image in enumerate(augmented_images):
                 output_path = os.path.join(output_folder, f"aug_{i}_{j}_{image_file}")
                 cv2.imwrite(output_path, augmented_image)
